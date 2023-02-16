7a3399b3f83ba61fe40f7363c24c7258148aecf5
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-16 11:36:26 +0000

    complete development environment

--- empowered/settings.py
+++ empowered/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = []
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
