ea7a819fe98c6c74b4e6bca5ad0752ef9670ffcc
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-17 02:15:02 +0000

    install allauth then set the settings up as well

--- empowered/urls.py
+++ empowered/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
