12c8421d847a8b19147cfadbccf6079fb310e486
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-18 11:47:11 +0000

    Add hero image: particles

---
+++ static/js/base.js
@@ -0,0 +1,9 @@
+window.onload= function() {
+    Particles.init({
+    selector:'.background',
+    color: '#bac0d5',
+    maxParticles: 180,
+    minDistance: 150,
+    connectParticles: true
+    });
+};
