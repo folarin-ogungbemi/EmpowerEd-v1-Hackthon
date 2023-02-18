460447c297d6d160e0775b3af949f4dd5fdc376b
oks-erm <oks.erm.012@gmail.com>
2023-02-18 20:44:54 +0000

    Add sending message and handling events in the consumers.py

--- chat_backend/consumers.py
+++ chat_backend/consumers.py
@@ -1,12 +1,11 @@
 from channels.generic.websocket import JsonWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(JsonWebsocketConsumer):
     """
-    This consumer is used to show user's online status,
-    and send notifications.
+    This consumer is used handle chat messages between users.
     """
-
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.room_name = None
@@ -15,10 +14,16 @@ class ChatConsumer(JsonWebsocketConsumer):
         print("Connected!")
         self.room_name = "home"
         self.accept()
+        # because group_add is asynchronous, it would not work
+        # without being converted to a synchronous function
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name,
+        )
         self.send_json(
             {
                 "type": "welcome_message",
-                "message": "Hey there! You've successfully connected!",
+                "message": "Hey, silly sausage! You've successfully connected!",
             }
         )
 
@@ -27,5 +32,18 @@ class ChatConsumer(JsonWebsocketConsumer):
         return super().disconnect(code)
 
     def receive_json(self, content, **kwargs):
-        print(content)
+        message_type = content["type"]
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                },
+            )
         return super().receive_json(content, **kwargs)
+    
+    def chat_message_echo(self, event):
+        print(event)
+        self.send_json(event)
