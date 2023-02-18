a7a95fee909652d089dd8712f755ac3423ede39a
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-18 14:33:38 +0000

    Style hero content and button

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,8 @@
 window.onload= function() {
     Particles.init({
-    selector:'.background',
+    selector:'.hero-bg',
     color: '#bac0d5',
-    maxParticles: 180,
+    maxParticles: 90,
     minDistance: 150,
     connectParticles: true
     });
