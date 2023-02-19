3790aaeab878e3723b0f305d0832515ec3638a49
oks-erm <oks.erm.012@gmail.com>
2023-02-19 19:07:58 +0000

    Fix profile

--- home/models.py
+++ home/models.py
@@ -53,7 +53,8 @@ class Parent(models.Model):
     parent_id = models.AutoField(primary_key=True)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
-    userpic = models.CharField(max_length=255, blank=True)
+    userpic = models.CharField(max_length=255, blank=True,
+                               default='https://i.imgur.com/0ndtScF.png')
 
     def __str__(self):
         return f'{self.user_id}'
@@ -64,7 +65,9 @@ class Student(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(default=timezone.now)
     interests = models.TextField()
-    parent = models.ForeignKey(Parent, on_delete=models.SET_NULL, null=True, blank=True)
+    parent = models.ForeignKey(Parent, on_delete=models.SET_NULL,
+                               null=True, blank=True, 
+                               default='https://i.imgur.com/0ndtScF.png')
     userpic = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
