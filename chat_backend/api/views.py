0e82f0c46ed5383b01d20936af8ff697d626ab61
oks-erm <oks.erm.012@gmail.com>
2023-02-19 13:06:18 +0000

    Update api views

--- chat_backend/api/views.py
+++ chat_backend/api/views.py
@@ -1,12 +1,12 @@
+"""
+Views of messenger api.
+"""
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
-from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.db.models import Q
 from django.db.utils import DatabaseError
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from home.models import User
 from chat_backend.api.pagynaters import MessagePagination
 from chat_backend.api.serialisers import ConversationSerializer, MessageSerializer
 from chat_backend.models import Conversation, Message
@@ -22,6 +22,10 @@ class ConversationViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     lookup_field = "name"
 
     def get_queryset(self):
+        """
+        Return a queryset of conversations that contain
+        the current user's ID.
+        """
         try:
             # pylint: disable=no-member
             queryset = Conversation.objects.filter(
@@ -41,10 +45,16 @@ class ConversationViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
         return Response(serializer.data)
 
     def get_serializer_context(self):
+        """
+        Return the request and current user in the serializer context.
+        """
         return {"request": self.request, "user": self.request.user}
 
 
 class MessageViewSet(ListModelMixin, GenericViewSet):
+    """
+    A viewset for viewing and retrieving Message objects.
+    """
     serializer_class = MessageSerializer
     # pylint: disable=no-member
     queryset = Message.objects.none()
