098c6bb568367e1dc41a4f8ecce3585b30473506
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-21 17:31:14 +0000

    add missing tag

--- empowered/settings.py
+++ empowered/settings.py
@@ -114,19 +114,21 @@ ASGI_APPLICATION = "empowered.asgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#         }
+
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
         }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
